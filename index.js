57aa0db74d5e5c1f3a9a65eb7cc97e0f6798de2f
Santhosh-SNTS <networksnts@gmail.com>
2022-10-03 13:58:09 +0000

    test by purusoth

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!09')
+  response.send('Hello World purusoth')
 })
 
 app.listen(app.get('port'), function() {
